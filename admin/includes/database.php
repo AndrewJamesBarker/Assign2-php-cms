a5bcf9976ab09d3cd63d0db7076abb6049dbc89a
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-03-09 14:47:41 -0500

    db update

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -1,12 +1,13 @@
 <?php
 
 $connect = mysqli_connect(
-    "sql213.epizy.com",
-    "epiz_29666120",
-    "U9x2tTEPKECh",
-    "epiz_29666120_portfolio"
+    "sql200.epizy.com",
+    "epiz_33366313",
+    "1juNvblGQ7",
+    "epiz_33366313_ufo_sightings"
  );
  
 
 mysqli_set_charset( $connect, 'UTF8' );
-// 1juNvblGQ7
+
+?>
