15c7a142e4137cdce6874b56bdfbe8fed01e7f86
Andrew Barker <103617658+Barkwanderer@users.noreply.github.com>
2023-03-09 12:31:16 -0500

    initial commit

---
+++ admin/includes/database.php
@@ -0,0 +1,11 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql200.epizy.com", 
+    "epiz_33366313", 
+    "1juNvblGQ7", 
+    "epiz_33366313_cms" 
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
+// 1juNvblGQ7
